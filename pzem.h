24c9a7ebe018bff03ec8558999045a8de79e201f
bramalka <74478922+bramalka@users.noreply.github.com>
2022-12-23 16:04:27 +0700

    Create pzem.h

---
+++ pzem.h
@@ -0,0 +1,15 @@
+#include "stm32f4xx.h"
+#include "main.h"
+
+typedef struct {
+	float voltage;
+	float current;
+	float power;
+	float energy;
+	float frequency;
+	float pf;
+	uint16_t alarms;
+} PZEM;
+
+void pzem_Init(UART_HandleTypeDef huart);
+void pzem_READ(PZEM *pzemhandle);
